35bb06b3b085640a9c2e6a0cda405ca0b473df7b
jorge <jgarcia@icebergsolutions.com>
2021-12-11 08:33:48 -0300

    topbar style

--- website/src/components/Topbar.tsx
+++ website/src/components/Topbar.tsx
@@ -5,9 +5,11 @@ import logoML from "../img/logo_ml.png";
 const Topbar: React.FC = () => {
   return (
     <div className="topbar_wrapper">
-      <div className="topbar">
-        <img className="topbar__logo" src={logoML} alt="Mercado Libre" />
-        <SearchBar />
+      <div className="container">
+        <div className="topbar">
+          <img className="topbar__logo" src={logoML} alt="Mercado Libre" />
+          <SearchBar />
+        </div>
       </div>
     </div>
   );
